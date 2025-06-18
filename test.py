f736da881e51ac9f284367344e7f45bb2235a672
MarkOsovets <markosovets@gmail.com>
2025-06-18 20:34:14 +0300

    Добавил тесты

---
+++ test.py
@@ -0,0 +1,10 @@
+from singleton import DataBase
+
+def test_singleton_instance():
+    db1 = DataBase("root", "1234", 80)
+    db2 = DataBase("admin", "pass", 404)
+    assert db1 is db2  # оба объекта — один и тот же экземпляр
+
+def test_read_data():
+    db = DataBase("root", "1234", 80)
+    assert db.read() == "данные из БД"
